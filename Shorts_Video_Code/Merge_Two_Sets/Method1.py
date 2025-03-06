f6a3b7c566595d602e5e524b3b20aac523107b5f
PyCodeArena <pycodearena@gmail.com>
2025-03-06 20:46:08 +0530

    feat: Added Dictionary Concatenation in Python

---
+++ Shorts_Video_Code/Merge_Two_Sets/Method1.py
@@ -0,0 +1,17 @@
+
+"""
+Adding Two Sets in Python
+"""
+
+# Method 1: Using | Union Operator
+
+set1 = {1, 2}
+set2 = {3, 4}
+
+Output = {1, 2, 3, 4}
+
+result = set1 | set2
+
+print(result)
+
+
---
+++ Shorts_Video_Code/Merge_Two_Sets/Method2.py
@@ -0,0 +1,16 @@
+
+"""
+Adding Two Sets in Python
+"""
+
+# Method 2: Using union() Method
+
+set1 = {1, 2}
+set2 = {3, 4}
+
+Output = {1, 2, 3, 4}
+
+result = set1.union(set2)
+
+print(result)
+
