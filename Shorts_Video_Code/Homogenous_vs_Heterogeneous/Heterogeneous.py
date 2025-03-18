1ce40647efa25d1abc42fe1804304dab605bcfd2
PyCodeArena <pycodearena@gmail.com>
2025-03-19 02:41:14 +0530

    feat: Added Homogenous_vs_Heterogeneous Data Types in Python

---
+++ Shorts_Video_Code/Homogenous_vs_Heterogeneous/Heterogeneous.py
@@ -0,0 +1,15 @@
+
+""" Heterogeneous Data Types Example """
+
+# List with mixed data types
+mixed_list = ["Python", 26, [1, 2, 3]]
+
+# Tuple with different data types
+mixed_tuple = ("Dragon", True, 42, 5.6)
+
+# Set with mixed immutable types
+mixed_set = {11, "PyCodeArena", 3.14}
+
+# Dictionary with mixed key-value pairs
+mixed_dict = {1: "One", 3.14: [1, 2]}
+
---
+++ Shorts_Video_Code/Homogenous_vs_Heterogeneous/Homogeneous.py
@@ -0,0 +1,15 @@
+
+""" Homogeneous Data Types Example """
+
+# List of integers
+numbers = [10, 20, 30, 40]
+
+# Tuple of strings
+words = ("Python", "JS", "C++")
+
+# Set of floats
+float_values = {1.1, 2.2, 3.3}
+
+# Dictionary with int keys & int values
+student_marks = {1: 85, 2: 90, 3: 78}
+
